926a0bbc0dfbcc8682233e842b0b33db1c661a38
bulut <bulutgozubuyuk@gmail.com>
2021-06-14 17:25:07 +0300

    exec ss and parse

--- src/modules/ThreadedTCPServerKeepPartnersAlive.cpp
+++ src/modules/ThreadedTCPServerKeepPartnersAlive.cpp
@@ -4,6 +4,49 @@
 
 #include "headers/servers/ThreadedTCPServerKeepPartnersAlive.h"
 
+std::string qx(const std::vector<std::string>& args) {
+    int p[2];
+    pipe(p);
+
+    const pid_t pid = fork();
+    if (!pid) {
+        close(p[0]);
+        dup2(p[1], 1);
+//        std::stderr << close(p[1]);
+        close(p[1]);
+
+        std::vector<char*> vec(args.size() + 1, 0);
+        for (size_t i = 0; i < args.size(); ++i) {
+            vec[i] = const_cast<char*>(args[i].c_str());
+        }
+
+
+
+        execvp(vec[0], &vec[0]);
+        exit(0);
+    }
+
+    close(p[1]);
+//    dup2(p[0], 0);
+
+    std::string out;
+    const int buf_size = 4096;
+    char buffer[buf_size];
+    do {
+        const ssize_t r = read(p[0], buffer, buf_size);
+        if (r > 0) out.append(buffer, r);
+    } while (errno == EAGAIN || errno == EINTR);
+
+    close(p[0]);
+
+    int r, status;
+    do {
+        r = waitpid(pid, &status, 0);
+    } while (r == -1 && errno == EINTR);
+
+    return out;
+}
+
 
 void ThreadedTCPServerKeepPartnersAlive::runPartnerChecker(void *obj_param) {
     ThreadedTCPServerKeepPartnersAlive *tcpServer = ((ThreadedTCPServerKeepPartnersAlive *) obj_param);
@@ -33,6 +76,30 @@ void ThreadedTCPServerKeepPartnersAlive::runPartnerChecker(void *obj_param) {
                 pid_t currentPid = tcpServer->startPartner();
                 (tcpServer->pidTimerMap).insert({currentPid, 0});
             }
+
+            sleep(1);
+
+            std::vector<std::string> vect;
+
+            vect.push_back("ls");
+            vect.push_back("-al");
+
+            std::string hheh = qx(vect);
+
+            printf(RED "QX: %s" RESET, hheh.c_str());
+
+            std::string colon = ":";
+            std::string space = " ";
+            std::string pid = "pid=";
+            std::string comma = ",";
+
+
+            std::string s = "127.0.1:12329 users:((Partner, pid=1132, fd=4))" ;
+            std::string prt = s.substr(s.find(colon) + 1, s.find(space)-s.find(colon)-1 );
+            s.erase(0, s.find(pid) + pid.length());
+            std::string pidno = s.substr(0, s.find(comma));
+
+            printf(YELLOW "Port>%s<  ---   Pid>%s<" RESET, prt.c_str(), pidno.c_str());
 
             pthread_mutex_unlock(&(tcpServer->_timer_mutex));
         }
